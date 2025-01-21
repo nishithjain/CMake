c39d016ff249f1cf4fe932413776c09fefa89fb5
Nishith Jain M R <write2nishi@gmail.com>
2025-01-21 22:41:04 +0530

    Updated .gitignore

---
+++ Practice/02MultipleFiles/my_lib.cpp
@@ -0,0 +1,7 @@
+#include "my_lib.h"
+#include <iostream>
+using namespace std;
+
+void print_hello_world(){
+    cout << "Hello World from Library\n";
+}
---
+++ Practice/02MultipleFiles/my_lib.h
@@ -0,0 +1,7 @@
+#ifndef MY_LIB_H
+#define MY_LIB_H
+
+// Declarations and definitions
+void print_hello_world();
+
+#endif // MY_LIB_H
