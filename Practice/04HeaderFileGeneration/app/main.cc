58a3038c169ef13cfdb2774f9769f6277327d701
Nishith Jain M R <write2nishi@gmail.com>
2025-01-23 18:37:20 +0530

    Updated notes and practice

---
+++ Practice/04HeaderFileGeneration/app/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "config.hpp"
+
+int main()
+{
+    std::cout << "Hello World\n";
+    std::cout << "Project Name:" << project_name << '\n';
+    std::cout << "Project Version:" << project_version << '\n';
+    return 0;
+}
